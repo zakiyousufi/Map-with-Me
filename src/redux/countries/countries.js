0ef76236e8b8f9db6eaa6ec3c00050fd8d1ddde2
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-10-05 00:25:20 +0430

    Update countries.js and import the api and fetch it

---
+++ src/redux/countries/countries.js
@@ -0,0 +1,9 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import api from '../api';
+import axios from 'axios';
+
+const FETCH_COUNTRIES = 'FETCH_COUNTRIES';
+const GET_COUNTRIES = 'GET_COUNTRIES';
+const initialState = [];
+
+export default countryReducer;

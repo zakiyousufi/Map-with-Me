00f3b9474b2356b0a5c7ad0373085faed7a545c4
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-10-06 01:37:49 +0430

    Install the axios into the project

--- src/components/Continents.js
+++ src/components/Continents.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import continents from '../data/data';
 import { useParams } from 'react-router';
 import { getCountries } from '../redux/countries/countries';
-import '../styles/continents.scss';
+import '../styles/continents.css';
 import { BsFillArrowRightSquareFill } from 'react-icons/bs';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -13,22 +13,23 @@ const Continents = () => {
   console.log('fdfd', countriesOfContinent);
   const handleClick = (name) => {
     dispatch(getCountries(name, idName));
-    return (
-      <div>
-        <ul className="continents">
-          {continents.map(({ id, name, image }) => (
-            <li key={id}>
-              {name}
-              <img src={image} />
-              <button type="button" onClick={() => handleClick(name)}>
-                <BsFillArrowRightSquareFill />
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
   };
-  };
+
+  return (
+    <div>
+      <ul className="continents">
+        {continents.map(({ id, name, image }) => (
+          <li key={id}>
+            {name}
+            <img src={image} />
+            <button type="button" onClick={() => handleClick(name)}>
+              <BsFillArrowRightSquareFill />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Continents;
--- src/redux/countries/countries.js
+++ src/redux/countries/countries.js
@@ -6,4 +6,26 @@ const FETCH_COUNTRIES = 'FETCH_COUNTRIES';
 const GET_COUNTRIES = 'GET_COUNTRIES';
 const initialState = [];
 
+
+const countryReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'FETCH_COUNTRIES/fulfilled':
+      return action.payload;
+    case 'GET_COUNTRIES/fulfilled':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export const fetchCountries = createAsyncThunk(FETCH_COUNTRIES, async () => {
+  const response = await axios.get(api);
+  return response.data;
+});
+
+export const getCountries = createAsyncThunk(GET_COUNTRIES, async (name) => {
+  const response = await axios.get(api);
+  return response.data.filter((country) => country.region == name);
+});
+
 export default countryReducer;

1c2cc86da64e7b7841f3154dfe0b94c8eef1eb76
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-10-06 13:26:19 +0430

    Fix the eslints errors

--- src/App.js
+++ src/App.js
@@ -19,24 +19,3 @@ function App() {
 }
 
 export default App;
-import { Route, Routes } from 'react-router';
-import './App.css';
-import WorldPage from './components/WorldPage';
-import Countries from './components/Countries';
-import Navbar from './components/Navbar';
-import CountryDetails from './components/CountryDetails';
-
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<WorldPage />} />
-        <Route path="/:name" element={<Countries />} />
-        <Route path="/:name/:id" element={<CountryDetails />} />
-      </Routes>
-    </div>
-  );
-}
-
-export default App;

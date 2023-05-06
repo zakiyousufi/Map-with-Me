78f9d16e92dfc5d9f3a5e729f6d44b6a5019bfc4
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-05-06 19:09:47 +0430

    Add loader

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import Loading from './Loading';
+import { Audio } from  'react-loader-spinner'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import { fetchCountryDetails } from '../redux/countries/countries';
 import '../styles/countries.scss';
 
@@ -14,42 +16,90 @@ const Countries = () => {
   };
   if (countries?.length === 0){
     return(
-      <Loading />
+      <div className='loader-container'>
+      <Audio
+        height = "40"
+        width = "40"
+        radius = "9"
+        color = '#fff'
+        ariaLabel = 'three-dots-loading'     
+        wrapperStyle
+        wrapperClass
+      />
+      Loading...
+    </div>
     )
   } else {
   return (
     <div className="countries">
+      <h2 className='select-country'>for more details Select a country</h2>
       <ul className="row">
         {' '}
         <>
+        <Carousel
+          additionalTransfrom={3}
+          arrows
+          autoPlay={true}
+          autoPlaySpeed={2000}
+          centerMode={true}
+          className="slider"
+          draggable
+          infinite
+          keyBoardControl
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024
+              },
+              items: 6,
+              partialVisibilityGutter: 40
+            },
+            mobile: {
+              breakpoint: {
+                max: 600,
+                min: 0
+              },
+              items: 3,
+              partialVisibilityGutter: 30
+            },
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 600
+              },
+              items: 4,
+              partialVisibilityGutter: 40
+            }
+          }}
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={1}
+          swipeable
+        >
           {countries.map((country) => (
           <>
-          <Link to={`/:/${country?.name.common}`}>
-            <div className='container' onClick={() => handleClick(country?.name.common)}>
-              <li key={country?.name.common}>
-              <div className="list">
-                <h2>{country?.name.common}</h2>
-                
-                
+            <Link to={`/:/${country?.name.common}`}>
+              <div className='container' onClick={() => handleClick(country?.name.common)}>
+                <li key={country?.name.common}>
+                <div className="list">
+                  <h2>{country?.name.common}</h2>
+                </div>
+                <img src={country?.flags.png} alt="flag" />
+                <div className="item">
+                  <p>Capital:</p>
+                  <p>{country?.capital}</p>
+                </div>
+                <div className="item">
+                  <p>TimeZone:</p>
+                  <p>{country?.timezones[0]}</p>
+                </div>
+              </li>
               </div>
-              <img src={country?.flags.png} alt="flag" />
-              <div className="item">
-                <p>Capital:</p>
-                <p>{country?.capital}</p>
-              </div>
-              <div className="item">
-                <p>SubRegion:</p>
-                <p>{country?.subregion}</p>
-              </div>
-              <div className="item">
-                <p>TimeZone:</p>
-                <p>{country?.timezones[0]}</p>
-              </div>
-            </li>
-            </div>
             </Link>
-            </>
+          </>
           ))}
+        </Carousel>
         </>
       </ul>
     </div>

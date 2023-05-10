c9d38e56831f080902bf453b7c54dcfe38a0eca1
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-05-10 22:27:25 +0430

    Removed slider from contries page

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { Audio } from  'react-loader-spinner'
-import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { fetchCountryDetails } from '../redux/countries/countries';
 import '../styles/countries.scss';
@@ -36,70 +35,28 @@ const Countries = () => {
       <ul className="row">
         {' '}
         <>
-        <Carousel
-          additionalTransfrom={3}
-          arrows
-          autoPlay={true}
-          autoPlaySpeed={2000}
-          centerMode={true}
-          className="slider"
-          draggable
-          infinite
-          keyBoardControl
-          responsive={{
-            desktop: {
-              breakpoint: {
-                max: 3000,
-                min: 1024
-              },
-              items: 6,
-              partialVisibilityGutter: 40
-            },
-            mobile: {
-              breakpoint: {
-                max: 600,
-                min: 0
-              },
-              items: 3,
-              partialVisibilityGutter: 30
-            },
-            tablet: {
-              breakpoint: {
-                max: 1024,
-                min: 600
-              },
-              items: 4,
-              partialVisibilityGutter: 40
-            }
-          }}
-          showDots={false}
-          sliderClass=""
-          slidesToSlide={1}
-          swipeable
-        >
           {countries.map((country) => (
           <>
-            <Link to={`/:/${country?.name.common}`}>
-              <div className='container' onClick={() => handleClick(country?.name.common)}>
-                <li key={country?.name.common}>
-                <div className="list">
-                  <h2>{country?.name.common}</h2>
-                </div>
-                <img src={country?.flags.png} alt="flag" />
-                <div className="item">
-                  <p>Capital:</p>
-                  <p>{country?.capital}</p>
-                </div>
-                <div className="item">
-                  <p>TimeZone:</p>
-                  <p>{country?.timezones[0]}</p>
-                </div>
-              </li>
+          <Link to={`/:/${country?.name.common}`}>
+            <div className='container' onClick={() => handleClick(country?.name.common)}>
+              <li key={country?.name.common}>
+              <div className="list">
+                <h2>{country?.name.common}</h2>
               </div>
-            </Link>
+              <img src={country?.flags.png} alt="flag" />
+              <div className="item">
+                <p>Capital:</p>
+                <p>{country?.capital}</p>
+              </div>
+              <div className="item">
+                <p>TimeZone:</p>
+                <p>{country?.timezones[0]}</p>
+              </div>
+            </li>
+            </div>
+          </Link>
           </>
           ))}
-        </Carousel>
         </>
       </ul>
     </div>
